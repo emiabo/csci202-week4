4f72de61372e379e175ecaa48e682980b3823b89
Emilia Aboudara <naboudara@gmail.com>
2021-07-11 12:44:51 -0700

    Implemented SortCo minigame

--- README.md
+++ README.md
@@ -2,16 +2,18 @@
 
 > Create an webpage that transforms between two distinct perspectives or identities through the user's interaction with it. "Identity" could mean aspects of a personality, political perspectives, moods, cultural backgrounds, or anything else. "User interaction" could encompass click, hover, mouseout (leaving element), doubleclick, keypress, window resize, scroll, or anything else. Basically, your page should start in one mode, and look very different during or after user interaction. It is suggested that you make use of JQuery events and JQuery effects, though not all of these will be applicable.
 
-Two perspectives or identities...
+Wow JS can get harder than I thought.
 
 - Day vs night, work vs play
-- Day mode: light theme, work-themed minigame
-- Night mode: dark theme, glowing screen, Space Invaders clone?
+- Day mode: light theme, work-themed minigame (sorting objects)
+- Night mode: dark theme, neon-bordered screen, Space Invaders clone?
+
+## Future possibilities
 - Switching between the two happens automatically every 30 sec (phases can be skipped with FFWD button)
 - After being exposed to both, they start interfering with each other (gradually obscuring more of the play area)
 - Experiment with making controls wonky (controlling both sides at the same time?)
 
 - [x] [Figma prototype of project](https://www.figma.com/file/7DfntNoTMHhDOhbn1cFpGK/Daily-Grind?node-id=0%3A1)
-- [ ] Day mode
+- [x] Day mode
 - [ ] Night mode
 - [?] Interference effects (game windows starting to overlap and animate OR controlling both at same time)
--- js/sketch.js
+++ js/sketch.js
@@ -1,6 +1,108 @@
+let teko;
+let boxTri;
+let boxStar;
+let boxDia;
+let tri;
+let star;
+let dia;
+let pay = 0;
+let score = 0;
+let formatter = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'});
+let draggingStar = false;
+let draggingDia = false;
+let draggingTri = false;
+let night = false;
+function preload() {
+    teko = loadFont('css/Teko-Medium.ttf');
+    boxTri = loadImage('img/box-tri.png');
+    boxStar = loadImage('img/box-star.png');
+    boxDia = loadImage('img/box-dia.png');
+    tri = loadImage('img/tri.png');
+    star = loadImage('img/star.png');
+    dia = loadImage('img/dia.png');
+}
+
 function setup() {
     let window = createCanvas(800, 600);
     window.parent('gameWindow');
+    imageMode(CENTER);
+  }
 
-    background(50);
+  function draw() {
+    clear();
+    if (!night) {
+      //SortCo text
+      textFont(teko);
+      textSize(100);
+      textAlign(CENTER);
+      fill(200);
+      text('SortCo Ltd.', 400, 350);
+      
+      //Pay counter
+      fill(0);
+      textFont(teko);
+      textAlign(LEFT);
+      textSize(36);
+      text('PAY '+formatter.format(pay), 25, 50);
+      
+      //draw boxes
+      image(boxTri, 200, 500);
+      image(boxStar, 400, 500);
+      image(boxDia, 600, 500);
+
+      //draw shapes
+      if (draggingStar) {
+        image(star, mouseX, mouseY);
+        image(dia, 400, 200);
+        image(tri, 600, 200);
+      } else if (draggingDia) {
+        image(star, 200, 200);
+        image(dia, mouseX, mouseY);
+        image(tri, 600, 200);
+      } else if (draggingTri) {
+        image(star, 200, 200);
+        image(dia, 400, 200);
+        image(tri, mouseX, mouseY);
+      } else {
+        image(star, 200, 200);
+        image(dia, 400, 200);
+        image(tri, 600, 200);
+      }
+    } else {
+      //Night mode (first set the styling)
+    }
+    
+  }
+  function mousePressed(){
+    if (!night) {
+      if (dist(200, 200, mouseX, mouseY) < 50) {
+            draggingStar = true;
+          } else if (dist(400, 200, mouseX, mouseY) < 50) {
+            draggingDia = true;
+          } else if (dist(600, 200, mouseX, mouseY) < 50) {
+            draggingTri = true;
+          }
+    } else {
+      //Night mode
+    }
+  }
+
+  function mouseReleased() {
+    if (!night) {
+      if (draggingStar && dist(400, 500, mouseX, mouseY) < 60) {
+            pay = pay + 50;
+            draggingStar = false;
+          } else if (draggingDia && dist(600, 500, mouseX, mouseY) < 60) {
+            pay = pay + 50;
+            draggingDia = false;
+          } else if (draggingTri && dist(200, 500, mouseX, mouseY) < 60) {
+            pay = pay + 50;
+            draggingTri = false;
+          } else if (draggingStar || draggingDia || draggingTri) {
+            pay = pay - 100;
+            draggingStar = draggingDia = draggingTri = false;
+          }
+    } else {
+      //Night mode
+    }
   }
