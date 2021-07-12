47e99949c08692331cf379625f103d2af8e3af4e
Emilia Aboudara <naboudara@gmail.com>
2021-07-12 13:21:02 -0700

    Partially implemented space shooter game

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 > Create an webpage that transforms between two distinct perspectives or identities through the user's interaction with it. "Identity" could mean aspects of a personality, political perspectives, moods, cultural backgrounds, or anything else. "User interaction" could encompass click, hover, mouseout (leaving element), doubleclick, keypress, window resize, scroll, or anything else. Basically, your page should start in one mode, and look very different during or after user interaction. It is suggested that you make use of JQuery events and JQuery effects, though not all of these will be applicable.
 
-Wow JS can get harder than I thought.
+Wow this got hairy fast.
 
 - Day vs night, work vs play
-- Day mode: light theme, work-themed minigame (sorting objects)
-- Night mode: dark theme, neon-bordered screen, Space Invaders clone?
+- Day mode: light theme, work-themed minigame (sorting objects at a packing facility)
+- Night mode: dark theme, neon-bordered screen, Space Invaders-like
 
 ## Future possibilities
 - Switching between the two happens automatically every 30 sec (phases can be skipped with FFWD button)
@@ -16,4 +16,5 @@ Wow JS can get harder than I thought.
 - [x] [Figma prototype of project](https://www.figma.com/file/7DfntNoTMHhDOhbn1cFpGK/Daily-Grind?node-id=0%3A1)
 - [x] Day mode
 - [ ] Night mode
-- [?] Interference effects (game windows starting to overlap and animate OR controlling both at same time)
+- [ ] Auto-switch timer
+- [ ] Interference effects (game windows starting to overlap and animate OR controlling both at same time)
--- js/sketch.js
+++ js/sketch.js
@@ -5,13 +5,24 @@ let boxDia;
 let tri;
 let star;
 let dia;
-let pay = 0;
+let ship;
+let pay = 0.0;
 let score = 0;
+let shipHeight = 300;
+let bulletX = 75;
+let bulletY = 300;
+let enemyA = true;
+let enemyB = true;
+let enemyC = true;
+let enemyD = true;
+let enemyE = true;
+let enemyF = true;
 let formatter = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'});
+let body;
 let draggingStar = false;
 let draggingDia = false;
 let draggingTri = false;
-let night = false;
+let night = true;
 function preload() {
     teko = loadFont('css/Teko-Medium.ttf');
     boxTri = loadImage('img/box-tri.png');
@@ -20,12 +31,15 @@ function preload() {
     tri = loadImage('img/tri.png');
     star = loadImage('img/star.png');
     dia = loadImage('img/dia.png');
+    ship = loadImage('img/polyspaceship.png');
 }
 
 function setup() {
     let window = createCanvas(800, 600);
     window.parent('gameWindow');
     imageMode(CENTER);
+    rectMode(CENTER);
+    body = select('body');
   }
 
   function draw() {
@@ -37,7 +51,7 @@ function setup() {
       textAlign(CENTER);
       fill(200);
       text('SortCo Ltd.', 400, 350);
-      
+
       //Pay counter
       fill(0);
       textFont(teko);
@@ -45,6 +59,12 @@ function setup() {
       textSize(36);
       text('PAY '+formatter.format(pay), 25, 50);
       
+      // Advance arrows
+      textFont('Nunito, sans-serif');
+      textSize(48);
+      textAlign(RIGHT);
+      text('>>', 775, 50);
+
       //draw boxes
       image(boxTri, 200, 500);
       image(boxStar, 400, 500);
@@ -70,6 +90,44 @@ function setup() {
       }
     } else {
       //Night mode (first set the styling)
+      body.class("night");
+      //Score counter
+      fill(255);
+      textFont('Lucida Console, monospace');
+      textAlign(LEFT);
+      textSize(36);
+      text('SCORE '+ score, 25, 50);
+      // Advance arrows
+      textFont('Nunito');
+      textSize(48);
+      textAlign(RIGHT);
+      text('>>', 775, 50);
+      //draw spaceship and boxes (static)
+      image(ship, 50, shipHeight);
+      fill('#CC0000');
+      if (enemyA) {
+        rect(600, 225, 50, 50);
+      }
+      if (enemyB) {
+        rect(600, 375, 50, 50);
+      }
+      if (enemyC) {
+        rect(600, 525, 50, 50);
+      }
+      if (enemyD) {
+        rect(750, 150, 50, 50);
+      }
+      if (enemyE) {
+        rect(750, 300, 50, 50);
+      }
+      if (enemyF) {
+        rect(750, 450, 50, 50);
+      }
+      if (!enemyA && !enemyB && !enemyC && !enemyD && !enemyE && !enemyF) {
+        enemyA = enemyB = enemyC = enemyD = enemyE = enemyF = true;
+      }
+      //check for keyboard input and increment or decrement shipHeight
+      
     }
     
   }
@@ -82,27 +140,38 @@ function setup() {
           } else if (dist(600, 200, mouseX, mouseY) < 50) {
             draggingTri = true;
           }
-    } else {
-      //Night mode
     }
   }
 
   function mouseReleased() {
     if (!night) {
-      if (draggingStar && dist(400, 500, mouseX, mouseY) < 60) {
-            pay = pay + 50;
+      if (draggingStar && dist(400, 500, mouseX, mouseY) < 100) {
+            pay = pay + 0.5;
             draggingStar = false;
-          } else if (draggingDia && dist(600, 500, mouseX, mouseY) < 60) {
-            pay = pay + 50;
+          } else if (draggingDia && dist(600, 500, mouseX, mouseY) < 100) {
+            pay = pay + 0.5;
             draggingDia = false;
-          } else if (draggingTri && dist(200, 500, mouseX, mouseY) < 60) {
-            pay = pay + 50;
+          } else if (draggingTri && dist(200, 500, mouseX, mouseY) < 100) {
+            pay = pay + 0.5;
             draggingTri = false;
           } else if (draggingStar || draggingDia || draggingTri) {
-            pay = pay - 100;
+            pay = pay - 1.0;
             draggingStar = draggingDia = draggingTri = false;
+          } else if (dist(745, 40, mouseX, mouseY) < 30){
+            night = true;
           }
     } else {
       //Night mode
+      if (dist(745, 40, mouseX, mouseY) < 30) {
+        night = false;
+        body.class('');
+      }
     }
+  }
+
+  function shoot() {
+    //Has to fire one bullet (or multiple) and move horizontally along screen until it hits an enemy, or the right edge. 
+    //If it hits, set that enemy to false and add 500 points. If not, subtract 500 points.
+    fill('#00FF38');
+    circle(bulletX, bulletY, 10);
   }
